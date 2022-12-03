a533a9c5a0f38d665bbfaef66a14d0f8b2394f41
Dasith Kuruppu <Dasithkuruppu@gmail.com>
2022-12-03 17:44:47 +0530

    Revert "style(Pretty Up Bot & Embed Output #81): Minor update for missed out variable"
    This reverts commit f433361be083e99a9002f2d75942993a3beeff4a.

--- bot/interactions/messageComponents/buttons/raidButton/confirm.ts
+++ bot/interactions/messageComponents/buttons/raidButton/confirm.ts
@@ -93,7 +93,7 @@ export const confirmButtonInteract = async (
           (userRecord as EmbedField)?.name ||
           PersistedClassInfo.className ||
           (defaultClass?.value as string),
-        optionalClasses: userExists ? optionalClasses : PersistedClassInfo?.optionalClasses
+        optionalClasses: optionalClasses.length ? optionalClasses : PersistedClassInfo.optionalClasses
       },
       { classNamesList: getOptionsList() }
     ),
